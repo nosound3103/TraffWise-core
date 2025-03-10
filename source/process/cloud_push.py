aa698786c94046a64d0c4381d2809d8fbbc879d3
Minh Tran <butdobin10@gmail.com>
2025-03-10 10:12:40 +0700

    Adding new function

--- source/process/cloud_push.py
+++ source/process/cloud_push.py
@@ -1,6 +1,7 @@
 import os
 import cloudinary
 import cloudinary.uploader
+import cloudinary.api
 from datetime import datetime
 import cv2
 from dotenv import load_dotenv
@@ -18,16 +19,14 @@ class AsyncCloudinaryUploader:
             api_secret=os.getenv('CLOUDINARY_API_SECRET')
         )
 
-    def _upload_to_cloudinary(self, frame):
+    def _upload_to_cloudinary(self, frame, public_id, folder_path):
         try:
             _, img_encoded = cv2.imencode('.jpg', frame)
-            timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-            public_id = f"violations/red_light_{timestamp}"
 
             result = cloudinary.uploader.upload(
                 img_encoded.tobytes(),
                 public_id=public_id,
-                folder="traffic_violations",
+                folder=folder_path,
                 resource_type="image"
             )
 
@@ -43,5 +42,18 @@ class AsyncCloudinaryUploader:
                 'error': str(e)
             }
 
-    def upload_violation(self, frame):
-        self.executor.submit(self._upload_to_cloudinary, frame.copy())
+    def upload_violation(self, frame, public_id, folder_path):
+        self.executor.submit(self._upload_to_cloudinary, frame.copy(), public_id, folder_path)
+
+    def file_exists_on_cloudinary(self, public_id_prefix):
+        """Check for exists file in Cloudinary."""
+        try:
+            response = cloudinary.api.resources(
+                type="upload",
+                prefix=public_id_prefix,
+                max_results=1,
+            )
+            return len(response["resources"]) > 0
+        except Exception as e:
+            print(f"Error checking file on Cloudinary: {e}")
+            return False
